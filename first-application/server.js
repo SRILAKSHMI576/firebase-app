4a06c5f7efb53da86b050adca063fd095a1e8639
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-01-26 22:46:12 +0530

    Create Authentication

--- first-application/server.js
+++ first-application/server.js
@@ -2,6 +2,15 @@ var express = require("express");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("");
+
+var firebaseAdmin = admin.initializeApp({
+  //cert means certify here
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: ""
+});
 //create instance of express app
 var app = express();
 
@@ -19,10 +28,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger("dev"));
 
+//create authentication middleware
+function isAuthenticated(request, response, next) {
+  //check user is logged in
+  //if they are, attach them to the request object and call next
+  // if they are not, send them to the login page
+  // with  a message saying : "login!"
+}
+
 app.get("/", function(req, res) {
   res.render("home.ejs");
 });
 
+app.get("/homecoming-queen", isAuthenticated, function(request, response) {
+  response.render("homecomingQueen.ejs");
+});
 app.post("/", function(req, res) {
   var breakfast = req.body.breakfast;
   res.render("results.ejs", { data: breakfast });
